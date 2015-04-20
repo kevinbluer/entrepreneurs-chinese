046ca47735e0ef393978115863d2bb3a89a0ceaf
Kevin Bluer <kevin@bluer.com>
2015-04-20 18:09:49 +0800

    init

--- js/app.js
+++ js/app.js
@@ -4,4 +4,24 @@ $(function() {
 		$("#title").text("Hello World");
 	});
 
+	$(".q1").click(function(e) {
+
+		// clear any existing results
+		$("#answer1").remove();
+
+		var result;
+
+		// get the value of the answer that the user clicked on and display an appropriate message
+		if ($(e.target).data("answer") === 2) {
+			result = '<div class="alert alert-success" role="alert" id="answer1"><strong>Yes!</strong> You are correct.</div>';
+		} else {
+			result = '<div class="alert alert-danger" role="alert" id="answer1"><strong>Sorry!</strong> Not this time.</div>';
+		}
+
+		// display the result
+		$("#question1").append(result);
+
+	});
+
 });
+
