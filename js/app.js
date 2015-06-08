e0aa5cafb860baeb8be9fad1868899ec1fc8eb33
Kevin Bluer <kevin@bluer.com>
2015-06-08 20:44:55 +0800

    some new stuff

--- js/app.js
+++ js/app.js
@@ -5,10 +5,15 @@ helloApp.config(function($routeProvider){
 		.when( '/home', { controller: 'HomeControlller', templateUrl: 'view-home.html' } )
 		.when( '/quiz', { controller: 'QuizControlller', templateUrl: 'view-quiz.html' } )
 		.when( '/contact', { controller: 'ContactControlller', templateUrl: 'view-contact.html' } )
+		.when( '/videos', { controller: 'VideosControlller', templateUrl: 'view-videos.html' } )
 		.otherwise( { redirectTo: '/home' } );
 })
 
 .controller('HomeControlller', function($scope) {
+  
+})
+
+.controller('VideosControlller', function($scope) {
   
 })
 
@@ -34,6 +39,44 @@ helloApp.config(function($routeProvider){
 		}
 	}
 
+	$scope.back = function() {
+		
+		switch (currentPage) {
+			case 1:
+				alert("You're already at the start!")
+				break;
+
+			case 2:
+				$scope.q1 = true;
+				$scope.q2 = false;
+				currentPage = 1;
+				break;
+
+			case 3:
+				$scope.q2 = true;
+				$scope.q3 = false;
+				currentPage = 2;
+				break;
+
+			case 4:
+				$scope.q3 = true;
+				$scope.q4 = false;
+				currentPage = 3;
+				break;				
+
+			case 5:
+				$scope.q4 = true;
+				$scope.q5 = false;
+				currentPage = 4;
+				break;
+
+			default:
+				alert("No more pages!")
+				break;
+		}
+
+	}
+
 	$scope.next = function() {
 
 		switch (currentPage) {
@@ -41,26 +84,28 @@ helloApp.config(function($routeProvider){
 				$scope.q1 = false;
 				$scope.q2 = true;
 				currentPage = 2;
-
 				break;
 
 			case 2:
 				$scope.q2 = false;
 				$scope.q3 = true;
 				currentPage = 3;
-
 				break;
 
 			case 3:
 				$scope.q3 = false;
 				$scope.q4 = true;
 				currentPage = 4;
-
 				break;
+
+			case 4:
+				$scope.q4 = false;
+				$scope.q5 = true;
+				currentPage = 5;
+				break;				
 
 			default:
 				alert("No more pages!")
-
 				break;
 		}
 	}
