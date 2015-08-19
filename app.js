cef736488b9e51002f07de7e25a78f82a1fba7a1
Kevin Bluer <kevin@bluer.com>
2015-08-19 22:09:36 +0800

    background cover

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   var head, body, footer;
   head = '<html><head><link href="http://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" type="text/css"></head>';
-  body = '<body style="background-image: url(http://cdn.bluer.com/images/shanghai-skyline.jpg); background-repeat: no-repeat;"><h2 style="color: white; text-align: center; font-family: Raleway;"><strong>Entrepreneur\'s</strong> Chinese</h2></body>';
+  body = '<body style="background-image: url(http://cdn.bluer.com/images/shanghai-skyline.jpg); background-size: cover;"><h2 style="color: white; text-align: center; font-family: Raleway;"><strong>Entrepreneur\'s</strong> Chinese</h2></body>';
   footer = '</html>';
   res.end(head + body + footer);
 }).listen(1337, '127.0.0.1');
