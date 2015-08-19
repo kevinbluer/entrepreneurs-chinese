d3345503b413e3db63c1d08bd22930632793635b
Kevin Bluer <kevin@bluer.com>
2015-08-19 17:55:29 +0800

    Some additional enhancements

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+// var http = require('http');
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello World\n');
+// }).listen(1337, '127.0.0.1');
+// console.log('Server running at http://127.0.0.1:1337/');
+
 var http = require('http');
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var head, body, footer;
+  head = '<html><head><link href="http://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" type="text/css"></head>';
+  body = '<body style="background-image: url(http://cdn.bluer.com/images/shanghai-skyline.jpg); background-repeat: no-repeat;"><h2 style="color: white; text-align: center; font-family: Raleway;"><strong>Entrepreneur\'s</strong> Chinese</h2></body>';
+  footer = '</html>';
+  res.end(head + body + footer);
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
