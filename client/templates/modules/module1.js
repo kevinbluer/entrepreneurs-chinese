72090b507334425d62f71135d65e668c5cfc3d58
Kevin Bluer <kevin@bluer.com>
2015-10-28 18:57:54 +0800

    adding some comments

--- client/templates/modules/module1.js
+++ client/templates/modules/module1.js
@@ -11,21 +11,25 @@ if (Meteor.isClient) {
 	Template.module1.events({
 		"click #next1": function() {
 
+			// get the current progress
 			var progress = Session.get("moduleProgress");
 
+			// update the progress (incrementing accordingly)
 			Session.set("moduleProgress", progress + 5);
 
-
+			// increment the user's score (note that we need to handle this properly)
 			if (isNaN(Session.get("userScore"))) {
 				Session.set("userScore", 5);
 			} else {
 				Session.set("userScore", Session.get("userScore") + 5);
 			}
 
+			// update the user's score
 			Meteor.users.update( { _id: Meteor.userId() }, { 
 				$set: { 'profile.score': Session.get("userScore") }
 			});
 
+			// hide the current panel (and show the next one)
 			$("#section1").hide();
 			$("#section2").show();
 
